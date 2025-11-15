ba969c06120bc5175f161d236056d47200479f15
Mridul Tripathi <mridultripathi869@gmail.com>
2025-11-15 12:13:08 +0530

    Create Majority Element.cpp

---
+++ ARRAYS 3/Majority Element.cpp
@@ -0,0 +1,16 @@
+int majorityElement(vector<int>& nums) {
+        int freq = 0;
+        int ans = 0;
+        for(int val : nums){
+            if(freq == 0){
+                ans = val;
+            }
+            if(ans == val){
+                freq++;
+            }
+            else{
+                freq--;
+            }
+        }
+        return ans;
+    }
