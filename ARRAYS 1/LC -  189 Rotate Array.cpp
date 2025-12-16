4c1d33addf13312a1d06c9ba0ed035f7521e6ff7
Mridul Tripathi <mridultripathi869@gmail.com>
2025-12-16 09:36:54 +0530

    Fix reverse function call for rotating array

--- ARRAYS 1/LC -  189 Rotate Array.cpp
+++ ARRAYS 1/LC -  189 Rotate Array.cpp
@@ -3,7 +3,5 @@ void rotate(vector<int>& nums, int k) {
         k = k%n;
         reverse(nums.begin(),nums.end());
         reverse(nums.begin(),nums.begin()+k);
-        reverse(nums.begin()+k,nums.end());
-        
-        
+        reverse(nums.begin()+k,nums.end());     
     }
